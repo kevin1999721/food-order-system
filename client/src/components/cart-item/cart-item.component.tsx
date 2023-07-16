3c300e9872411e44eb7ab902293c2f2dba820aaf
kevin1999721 <kevin1999721@gmail.com>
2023-07-17 02:52:22 +0800

    create cart and cart-item component
    - it can change cart items quantity in cart, and also remove whole cart item
    - create submit button for post order list to server

---
+++ client/src/components/cart-item/cart-item.component.tsx
@@ -0,0 +1,70 @@
+import { FC } from 'react';
+import { useAppDispatch } from '../../store/hooks';
+import { changeCartItemQuantityByID, removeItemFromCartById } from '../../store/cart/cart.slice';
+import { CartItem as CartItemType } from '../../store/cart/cart.slice';
+
+import { Box, IconButton, Typography } from '@mui/material';
+
+import CloseIcon from '@mui/icons-material/Close';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+type CartItemProps = {
+	cartItem: CartItemType;
+};
+
+const CartItem: FC<CartItemProps> = ({ cartItem }) => {
+	const dispathch = useAppDispatch();
+	const { id, name, price, quantity } = cartItem;
+
+	const onRemoveButtonClick = () => {
+		dispathch(removeItemFromCartById(id));
+	};
+
+	const onIncreaseButtonClick = () => {
+		if (quantity < 100) dispathch(changeCartItemQuantityByID({ id, changeQuantity: 1 }));
+	};
+
+	const onDecreaseButtonClick = () => {
+		if (quantity > 1) dispathch(changeCartItemQuantityByID({ id, changeQuantity: -1 }));
+	};
+
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				alignItems: 'center',
+				gap: '10px',
+			}}
+		>
+			<Box>
+				<IconButton onClick={onRemoveButtonClick}>
+					<CloseIcon />
+				</IconButton>
+			</Box>
+			<Box>
+				<Typography fontWeight={600}>{`${name} $${price}`}</Typography>
+			</Box>
+			<Box
+				sx={{
+					display: 'flex',
+					alignItems: 'center',
+					gap: '5px',
+					marginLeft: 'auto',
+				}}
+			>
+				<IconButton onClick={onDecreaseButtonClick}>
+					<RemoveIcon />
+				</IconButton>
+				<Typography fontWeight={600} width={'20px'} textAlign={'center'}>
+					{quantity}
+				</Typography>
+				<IconButton onClick={onIncreaseButtonClick}>
+					<AddIcon />
+				</IconButton>
+			</Box>
+		</Box>
+	);
+};
+
+export default CartItem;
---
+++ client/src/components/cart/cart.component.tsx
@@ -0,0 +1,84 @@
+import { useAppSelector, useAppDispatch } from '../../store/hooks';
+import {
+	selectCartItems,
+	selectCartItmesCount,
+	selectCartItmesTotal,
+} from '../../store/cart/cart.select';
+import { selectStoreById } from '../../store/stores/stores.select';
+import { thunkPostOrderList } from '../../store/order-lists/order-lists.slice';
+
+import { Box, Typography, Divider, Button } from '@mui/material';
+
+import CartItem from '../cart-item/cart-item.component';
+
+const Cart = () => {
+	const disaptch = useAppDispatch();
+	const store = useAppSelector(selectStoreById);
+	const cartItems = useAppSelector(selectCartItems);
+	const cartItemsCount = useAppSelector(selectCartItmesCount);
+	const cartItemsTotal = useAppSelector(selectCartItmesTotal);
+
+	const onSubmitButtonClick = () => {
+		if (store && cartItemsCount > 0 && cartItemsTotal > 0) {
+			disaptch(
+				thunkPostOrderList({
+					store: {
+						id: store.id,
+						name: store.name,
+					},
+					products: cartItems,
+					count: cartItemsCount,
+					total: cartItemsTotal,
+				})
+			);
+		}
+	};
+
+	return (
+		<Box sx={{ maxWidth: '100%', minWidth: '300px', width: '380px', padding: '20px 10px' }}>
+			<Box sx={{ height: '250px', overflow: 'auto' }}>
+				{cartItems.length > 0 ? (
+					cartItems.map(cartItem => <CartItem key={cartItem.id} cartItem={cartItem} />)
+				) : (
+					<Box
+						sx={{
+							display: 'flex',
+							flexDirection: 'column',
+							justifyContent: 'center',
+							alignItems: 'center',
+							width: '100%',
+							height: '100%',
+						}}
+					>
+						<Typography fontWeight={600} fontStyle={'italic'} color={'GrayText'}>
+							尚無商品
+						</Typography>
+					</Box>
+				)}
+			</Box>
+			<Divider />
+			<Box
+				sx={{
+					display: 'flex',
+					justifyContent: 'flex-end',
+					gap: '20px',
+					marginTop: '10px',
+					'& > p': { fontSize: '20px', fontWeight: '600' },
+				}}
+			>
+				<Typography>{`總數量 ${cartItemsCount}`}</Typography>
+				<Typography>{`總金額 $${cartItemsTotal}`}</Typography>
+			</Box>
+			<Button
+				onClick={onSubmitButtonClick}
+				disabled={cartItemsTotal <= 0}
+				variant="outlined"
+				sx={{ width: '100%', marginTop: '20px' }}
+			>
+				送出訂單
+			</Button>
+		</Box>
+	);
+};
+
+export default Cart;
